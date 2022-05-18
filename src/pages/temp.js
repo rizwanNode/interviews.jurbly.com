60d78b2e646edf1c8f0951a7d80476d826490ca6
rizwanNode <102730752+rizwanNode@users.noreply.github.com>
2022-05-18 12:52:20 +0500

    Update temp.js

--- src/pages/temp.js
+++ src/pages/temp.js
@@ -3,7 +3,7 @@ export default {
     "_id": {
       "$oid": "5c607c60b62fdb0008f5afd0"
     }, 
-    "email": "russell@deephire.com", 
+    "email": "max@jurbly.com", 
     "interviewName": "Sales Interview", 
     "interview_config": {
       "answerTime": 90, 
@@ -27,10 +27,10 @@ export default {
         "question": "Where do you see yourself in 5 years?"
       }
     ], 
-    "long_url": "https://interview.deephire.com/pickInterview/http%3A%2F%2Fdeeplink.me%2Finterview.deephire.com%2Fhome%3Fid%3D5c607c60b62fdb0008f5afd0%26flow%3Dinterview%26pin%3D85619", 
+    "long_url": "https://interview.jurbly.com/pickInterview/http%3A%2F%2Fdeeplink.me%2Finterview.deephire.com%2Fhome%3Fid%3D5c607c60b62fdb0008f5afd0%26flow%3Dinterview%26pin%3D85619", 
     "pin": "85619", 
     "python_datetime": "2019-02-10 14:32:48", 
-    "short_url": "http://link.deephire.com/2aa9n"
+    "short_url": "http://link.jurbly.com/2aa9n"
   }
 }
 
