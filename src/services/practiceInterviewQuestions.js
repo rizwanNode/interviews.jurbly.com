240c07fb09cddeac324a640e6d0ec5b022075aac
rizwanNode <102730752+rizwanNode@users.noreply.github.com>
2022-05-18 13:02:42 +0500

    Update practiceInterviewQuestions.js

--- src/services/practiceInterviewQuestions.js
+++ src/services/practiceInterviewQuestions.js
@@ -3,7 +3,7 @@ export default
     "_id": {
       "$oid": "5c607c60b62fdb0008f5afd0"
     }, 
-    "email": "russell@deephire.com", 
+    "email": "max@jurbly.com", 
     "interviewName": "Sales Interview", 
     "interviewConfig": {
       "answerTime": 90, 
@@ -15,10 +15,10 @@ export default
         "question": "(Practice) Tell me about yourself."
       }
     ], 
-    "longUrl": "https://interview.deephire.com/pickInterview/http%3A%2F%2Fdeeplink.me%2Finterview.deephire.com%2Fhome%3Fid%3D5c607c60b62fdb0008f5afd0%26flow%3Dinterview%26pin%3D85619", 
+    "longUrl": "https://interview.jurbly.com/pickInterview/http%3A%2F%2Fdeeplink.me%2Finterview.deephire.com%2Fhome%3Fid%3D5c607c60b62fdb0008f5afd0%26flow%3Dinterview%26pin%3D85619", 
     "pin": "85619", 
     "timestamp": "2019-02-10 14:32:48", 
-    "shortUrl": "http://link.deephire.com/2aa9n"
+    "shortUrl": "http://link.jurbly.com/2aa9n"
   }
 
 
