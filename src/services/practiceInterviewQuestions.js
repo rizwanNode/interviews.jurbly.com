368dc1377a18ca2242f8f8dba56f9c3799c9ef6b
rizwanNode <102730752+rizwanNode@users.noreply.github.com>
2022-05-18 13:03:03 +0500

    Update practiceInterviewQuestions.js

--- src/services/practiceInterviewQuestions.js
+++ src/services/practiceInterviewQuestions.js
@@ -15,7 +15,7 @@ export default
         "question": "(Practice) Tell me about yourself."
       }
     ], 
-    "longUrl": "https://interview.jurbly.com/pickInterview/http%3A%2F%2Fdeeplink.me%2Finterview.deephire.com%2Fhome%3Fid%3D5c607c60b62fdb0008f5afd0%26flow%3Dinterview%26pin%3D85619", 
+    "longUrl": "https://interview.jurbly.com/pickInterview/http%3A%2F%2Fdeeplink.me%2Finterview.jurbly.com%2Fhome%3Fid%3D5c607c60b62fdb0008f5afd0%26flow%3Dinterview%26pin%3D85619", 
     "pin": "85619", 
     "timestamp": "2019-02-10 14:32:48", 
     "shortUrl": "http://link.jurbly.com/2aa9n"
